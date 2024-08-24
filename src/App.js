52066e34a3b2b836206caf53a65a2bdea5151de4
sarthak mehta <sarthakmehta768@gmail.com>
2024-08-24 19:13:43 +0530

    client final commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './header';
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const jokes_data=()=>{
+  const [jokes,setJokes]=useState('')
+
+  const apifetch=async ()=>{
+    try{
+    const jokesdata=await axios.post(`${process.env.REACT_APP_JOKES_URL}`);
+    setJokes(jokesdata.data.joke)
     
+  }catch(err){
+    console.log(err)
   }
+  }
+
+  useEffect(() => {
+    apifetch();
+  }, []);
+
   return (
     <>
-    <Header/>
-    <h1>jokes</h1>
+      <Header />
+      <div>
+      <h1>Jokes</h1>
+      <h1>{jokes}</h1>
+      </div>
+      
     </>
   );
 }
